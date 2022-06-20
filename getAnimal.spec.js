970504ca8f9384f26052ef5ab2a3aa6c03b113fe
PReichetanz <paul.reichetanz@gmx.de>
2022-06-20 16:47:35 +0200

    Add getAnimal exercise

---
+++ getAnimal.spec.js
@@ -0,0 +1,12 @@
+import { getAnimal } from './getAnimal.js';
+
+describe('getAnimal', () => {
+  it.skip('returns a string "I really like cats!" if called with getAnimals("cat")', () => {});
+});
+
+/*
+
+- if getAnimal is called with "cat" as argument -> return "I really like cats!"
+- if getAnimal is called with an argument -> "I like [animal]!"
+- if called without animals -> "I don't like animals"
+*/
